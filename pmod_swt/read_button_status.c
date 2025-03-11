d3d34e4e60b18e7ae56ae5ab80235d30f6fce86a
gael chauvet, lucas hyot <mail>
2025-03-11 10:44:35 +0100

    modif du code C pour prendre en argument le port à lire

--- pmod_swt/read_button_status.c
+++ pmod_swt/read_button_status.c
@@ -1,19 +1,54 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-int main(char *argv[], int argc){
+int main(int argc,char *argv[]){
 
 
+    // for(int i = 0; i < argc; i++){
 
-    system("gpioget gpiochip0 08");
-    system("gpioget gpiochip0 10");
-    system("gpioget gpiochip0 09");
-    system("gpioget gpiochip0 11");
-    system("gpioget gpiochip0 19");
-    system("gpioget gpiochip0 21");
-    system("gpioget gpiochip0 20");
-    system("gpioget gpiochip0 18");
-		
+    //     printf("%s\n",argv[i]);
+
+    // }
+    if(strcmp(argv[1], "JA") == 0){
+        system("gpioget gpiochip0 08");
+        system("gpioget gpiochip0 10");
+        system("gpioget gpiochip0 09");
+        system("gpioget gpiochip0 11");
+        system("gpioget gpiochip0 19");
+        system("gpioget gpiochip0 21");
+        system("gpioget gpiochip0 20");
+        system("gpioget gpiochip0 18");
+        printf("JA read \n");
+    }
+    else if(strcmp(argv[1], "JB") == 0){
+        system("gpioget gpiochip0 07");
+        system("gpioget gpiochip0 10");
+        system("gpioget gpiochip0 09");
+        system("gpioget gpiochip0 11");
+        system("gpioget gpiochip0 26");
+        system("gpioget gpiochip0 13");
+        system("gpioget gpiochip0 03");
+        system("gpioget gpiochip0 02");
+        printf("JB read\n");
+    }
+    else if(strcmp(argv[1], "JC") == 0){
+        system("gpioget gpiochip0 16");
+        system("gpioget gpiochip0 14");
+        system("gpioget gpiochip0 15");
+        system("gpioget gpiochip0 17");
+        system("gpioget gpiochip0 04");
+        system("gpioget gpiochip0 12");
+        system("gpioget gpiochip0 05");
+        system("gpioget gpiochip0 06");
+        printf("JC read\n");
+    }
+    else{
+        printf("JA JB or JC expected\n");
+        printf(argv[1]);
+        printf(" found\n");
+    }		
 	
 
 	return 0;
